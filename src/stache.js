9b79f2a9d30a55bca29b57704b28a970cff0cc60
Ryan Morr <rm.morr@gmail.com>
2020-03-03 13:27:14 -0500

    Add support for if directives

--- src/stache.js
+++ src/stache.js
@@ -1,11 +1,36 @@
+const NEW_LINES_RE = /\r?\n/g;
+const TEMPLATE_RE = /{{\s*(.+?)\s*}}/g;
+const IF_RE = /^if (.*)$/;
+const ELSE_IF_RE = /^else if (.*)?$/;
+
+function parse(source) {
+    return source.replace(NEW_LINES_RE, '\\n').replace(TEMPLATE_RE, (all, code) => {
+        if (code.startsWith('if')) {
+			let conditional = (IF_RE).exec(code);
+			if (conditional) {
+				return `'); if (${conditional[1]}) { _.push('`;
+			}
+		} else if (code.startsWith('else if')) {
+			let conditionalElse = (ELSE_IF_RE).exec(code);
+			if (conditionalElse) {
+				return `'); } else if (${conditionalElse[1]}) { _.push('`;
+			}
+        } else if (code === 'else') {
+			return `'); } else { _.push('`;
+		} else if (code === '/if') {
+			return `'); } _.push('`;
+		}
+        return `',(${code}),'`;
+    });
+}
+
 export default function stache(source) {
     let tpl;
-    source = source.replace(/\r?\n/g, '\\n').replace(/{{\s*(.+?)\s*}}/g, (all, code) => `',(${code}),'`);
     return (data) => {
 		if (!tpl) {
             tpl = new Function(`
                 const _ = [], ${Object.keys(data).map((key) => `${key} = this['${key}']`).join(',')};
-                _.push('${source}');
+                _.push('${parse(source)}');
                 return _.join('')
             `);
 		}
--- test/specs/stache.js
+++ test/specs/stache.js
@@ -56,4 +56,119 @@ describe('stache', () => {
         });
         expect(result2).to.equal('17');
     });
+
+    it('should support an if directive', () => {
+        const tpl = stache(`
+            {{if foo}}
+                {{bar}}
+            {{/if}}
+        `);
+
+        const result1 = tpl({
+            foo: true,
+            bar: 'baz'
+        });
+        expect(result1.trim()).to.equal('baz');
+
+        const result2 = tpl({
+            foo: false,
+            bar: 'baz'
+        });
+        expect(result2.trim()).to.equal('');
+    });
+
+    it('should support an if-else directive', () => {
+        const tpl = stache(`
+            {{if foo}}
+                {{bar}}
+            {{else}}
+                {{baz}}
+            {{/if}}
+        `);
+
+        const result1 = tpl({
+            foo: true,
+            bar: 'abc',
+            baz: 123
+        });
+        expect(result1.trim()).to.equal('abc');
+
+        const result2 = tpl({
+            foo: false,
+            bar: 'abc',
+            baz: 123
+        });
+        expect(result2.trim()).to.equal('123');
+    });
+
+    it('should support an if-else-if directive', () => {
+        const tpl = stache(`
+            {{if foo}}
+                {{bar}}
+            {{else if baz < 10}}
+                {{baz + 10}}
+            {{else}}
+                {{qux}}
+            {{/if}}
+        `);
+
+        const result1 = tpl({
+            foo: true,
+            bar: 'abc',
+            baz: 5,
+            qux: true
+        });
+        expect(result1.trim()).to.equal('abc');
+
+        const result2 = tpl({
+            foo: false,
+            bar: 'abc',
+            baz: 5,
+            qux: true
+        });
+        expect(result2.trim()).to.equal('15');
+
+        const result3 = tpl({
+            foo: false,
+            bar: 'abc',
+            baz: 15,
+            qux: true
+        });
+        expect(result3.trim()).to.equal('true');
+    });
+
+    it('should support nested if directives', () => {
+        const tpl = stache(`
+            {{if foo}}
+                {{if bar === 10}}
+                    {{bar}}
+                {{else}}
+                    {{baz}}
+                {{/if}}
+            {{else}}
+                empty
+            {{/if}}
+        `);
+
+        const result1 = tpl({
+            foo: false,
+            bar: 10,
+            baz: 20
+        });
+        expect(result1.trim()).to.equal('empty');
+
+        const result2 = tpl({
+            foo: true,
+            bar: 10,
+            baz: 20
+        });
+        expect(result2.trim()).to.equal('10');
+
+        const result3 = tpl({
+            foo: true,
+            bar: 11,
+            baz: 20
+        });
+        expect(result3.trim()).to.equal('20');
+    });
 });
